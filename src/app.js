34b25d8650bbfebdab7275f2f6de315538d8f85c
Matt Watson <mwatson@renci.org>
2022-12-06 17:31:40 -0500

    initial commit from renci/react-starter & decruft

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import { Fragment } from 'react'
+import { Carousel } from './components/carousel'
+
+export const App = () => {
+  return (
+    <Fragment>
+      <Carousel />
+    </Fragment>
+  )
+}
---
+++ src/components/carousel/index.js
@@ -0,0 +1,9 @@
+import { Fragment } from 'react'
+
+export const Carousel = () => {
+  return (
+    <Fragment>
+      <h1>carousel!</h1>
+    </Fragment>
+  )
+}
