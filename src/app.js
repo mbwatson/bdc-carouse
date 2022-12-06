ef04facf70f1aaea17a8f25d1d3f16296b79b4a9
Matt Watson <mwatson@renci.org>
2022-12-06 18:20:16 -0500

    cleanup, add type-checking

--- src/app.js
+++ src/app.js
@@ -1,10 +1,54 @@
 import { Fragment } from 'react'
 import { Carousel } from './components/carousel'
 
+const carouselData = [
+  {
+    type: 'testimonial',
+    content: {
+      quote: 'Esse in veniam elit eu adipisicing ut elit dolor consequat in et laboris.',
+      attribution: 'Alice Bob',
+    },
+  },
+  {
+    type: 'stats',
+    content: {
+      headline: 'Ut pariatur dolore do dolore',
+      description: 'Irure in commodo ad voluptate non aliqua minim tempor qui officia ex laborum laboris officia magna duis consequat tempor.',
+      stats: [
+        {
+          name: 'thing 1',
+          value: 100
+        },
+        {
+          name: 'thing 2',
+          value: 200
+        },
+      ]
+    },
+  },
+  {
+    type: 'dataset-info',
+    content: {
+      headline: 'Adipisicing laboris culpa nulla',
+      description: 'Aute aute labore nisi dolor proident est enim labore consectetur labore.',
+      data: [
+        {
+          name: 'thing a',
+          value: 100
+        },
+        {
+          name: 'thing b',
+          value: 200
+        },
+      ]
+    }
+  },
+]
+
 export const App = () => {
   return (
     <Fragment>
-      <Carousel />
+      <Carousel data={ carouselData } />
     </Fragment>
   )
 }
--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -1,9 +1,53 @@
 import { Fragment } from 'react'
+import PropTypes from 'prop-types'
 
-export const Carousel = () => {
+export const Carousel = ({ data }) => {
   return (
     <Fragment>
       <h1>carousel!</h1>
+      <pre>{ JSON.stringify(data, null, 2) }</pre>
     </Fragment>
+  )
+}
+
+//
+
+const testomonialType = PropTypes.shape({
+  quote: PropTypes.string.isRequired,
+  attribution: PropTypes.string.isRequired,
+})
+
+const statsType = PropTypes.shape({
+  headline: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      value: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+})
+
+const datasetInfoType = PropTypes.shape({
+  headline: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      value: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+})
+
+Carousel.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.oneOf([
+        'testimonial', 'stats', 'dataset-info'
+      ]),
+      content: PropTypes.oneOfType([
+        testomonialType, statsType, datasetInfoType
+      ]).isRequired,
+    })
   )
 }
